f68a538a846acd5a63d4a0c975218a330d2c635a
taslima63 <ruba6315@gmail.com>
2022-05-06 23:57:28 +0900

    db connected

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Halal food server is Running ')
+})
+
+app.listen(port, () => {
+    console.log('Hello port', port);
+})
