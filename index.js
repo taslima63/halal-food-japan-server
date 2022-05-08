95a1d86e732c95bd6c511221af17e9facba20b2c
taslima63 <ruba6315@gmail.com>
2022-05-08 17:01:06 +0900

    .env file added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://authUser:T8eM7XnpthrLGT8K@cluster0.araz4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.araz4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
